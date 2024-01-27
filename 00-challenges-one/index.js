4b2374b15a1f687c8b3a8f4b831203008921a5b0
Edgar Jesus <mendesbr92@gmail.com>
2024-01-27 15:30:27 -0300

    feat: script esta funcional

---
+++ 00-challenges-one/index.js
@@ -0,0 +1,33 @@
+let p = document.getElementById('xxx');
+
+function capturarEntrada() {
+  let entradaUsuario = document.getElementById('entradaUsuario').value.toLowerCase();
+
+
+  let novastring = entradaUsuario
+  .replace(/i/g , 'imes')
+  .replace(/e/g, 'enter')
+  .replace(/a/g, 'ai')
+  .replace(/o/g, 'ober')
+  .replace(/u/g, 'ufat');
+
+  p.innerHTML = novastring;
+
+}
+
+function capturarSaida(){
+  let descText = document.getElementById('desc').value.toLowerCase();
+
+  let novaString = descText
+  .replace(/imenters/g,'i')
+  .replace(/enter/g,'e')
+  .replace(/ai/g, 'a')
+  .replace(/ober/g, 'o')
+  .replace(/ufat/g, 'u')
+
+  p.innerHTML = novaString;
+}
+
+
+
+
